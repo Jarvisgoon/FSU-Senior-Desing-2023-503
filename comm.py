027045cb0f7103b900dd13ce145381f044aaec88
DESKTOP-52OSS4A\djgor <djgor46@gmail.com>
2023-03-21 18:35:46 -0400

    Read send multiple bytes to teensy

--- comm.py
+++ comm.py
@@ -7,18 +7,22 @@ teensy = serial.Serial(outport, 9600)
 while True:
     # Code to generate output
     command = input("Bit: ")
+    command2 = input("Bit2: ")
     
     # Code to normalize output
     cmdByte = bytes(command, 'utf-8')
+    cmd2Byte = bytes(command2, 'utf-8')
     
     # send command
     try:
         teensy.write(cmdByte)
+        teensy.write(cmd2Byte)
         teensy.flush()
     except:
         teensy.close()
         print("Data send failed")
+        break
         
-    if command != '1' and command != '0':
+    if (command != '1' and command != '0') or (command2 != '1' and command2 != '0'):
         teensy.close()
         break
